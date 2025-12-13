2fef5fbbf557e7b72ce1ce73f6c4208f12aed381
Richclaud Felix <claudfelix030@gmail.com>
2025-12-13 15:14:31 +0800

    feature: 商品画像をアップロード機能を実装

---
+++ src/main/java/com/portfolio/spring_ecommerce/service/S3Service.java
@@ -0,0 +1,45 @@
+package com.portfolio.spring_ecommerce.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        String key = "products/" + UUID.randomUUID().toString() + fileExtension;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(key)).toExternalForm();
+    }
+
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.lastIndexOf('.') == -1) {
+            return "";
+        }
+        return fileName.substring(fileName.lastIndexOf('.'));
+    }
+}
